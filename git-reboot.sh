b4749be8ea4b12dadaeb8602fdea8ca3dd1e08f7
Dmitry Bizyaev <83307719+DmitryBizyaev@users.noreply.github.com>
2025-06-03 20:11:29 +0300

    Update test

---
+++ git-reboot.sh
@@ -0,0 +1,7 @@
+rm -rf .
+
+git clone https://github.com/DmitryBizyaev/personal-finances-telegram-bot.git
+
+cd /personal-finances-telegram-bot
+
+python test.py
--- test.py
+++ test.py
@@ -1,8 +1,26 @@
-import os
+# import os
+# import telebot
+# import secrets
+# from telebot import types
 
 def main():
-    Param = os.environ['PARAM']
-    print(Param)
+
+    # DATABASE_NAME = 'schedule.db'
+
+    # bot = telebot.TeleBot(secrets.token)
+
+
+    # @bot.message_handler(commands = ['start'])
+    # def start_message(message):
+
+    #     tg_user_id = str(message.from_user.id)
+
+    #     bot.send_message(message.chat.id, message.from_user.id)
+
+    # bot.infinity_polling()
+
+    while 1:
+        print("test1")
         
 
 if __name__ == "__main__":
